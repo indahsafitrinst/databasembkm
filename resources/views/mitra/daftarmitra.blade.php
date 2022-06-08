ad29534c7c1730bb03446bfc6309da0678944b86
indahsafitrinst <indahhsafitri161201.gmail.com>
2022-06-09 06:12:41 +0700

    mitra

---
+++ resources/views/mitra/daftarmitra.blade.php
@@ -0,0 +1,234 @@
+@extends('template.layouttempdsn')
+@section('sidebar')
+<nav id="mySidebar" class=" d-lg-block sidebar bg-white">
+  <div class="position-sticky">
+    <div class="list-group list-group-flush mx-3">
+      <div class="d-flex justify-content-end align-items-center">
+        <i href="javascript:void(0)" class="bi bi-x-square closebtn btn btn-sm " onclick="closeNav()"></i>
+      </div>
+      <a href="dashboarddsn" class="list-group-item list-group-item-action my-2 py-2 ripple">
+        <i class="bi bi-square-half me-3"></i>
+        <span>Dashboard</span>
+      </a>
+      <a href="permohonanmbkm" class="list-group-item list-group-item-action my-2 py-2 ripple">
+        <i class="bi bi-card-checklist me-3 "></i>
+        <span>Permohonan Program MBKM</span>
+      </a>
+      <a href="konversinilaikhs" class="list-group-item list-group-item-action my-2 py-2 ripple">
+        <i class="bi bi-postcard-fill me-3"></i>
+        <span>Konversi Nilai Kartu Hasil Studi</span>
+      </a>
+      <a href="mhsdibimbing" class="list-group-item list-group-item-action my-2 py-2 ripple">
+        <i class="bi bi-people-fill me-3"></i>
+        <span>Mahasiswa Dibimbing</span>
+      </a>
+      <a href="dkmndikirim" class="list-group-item list-group-item-action my-2 py-2 ripple">
+        <i class="bi bi-file-earmark-check me-3"></i>
+        <span>Dokumen Dikirim</span>
+      </a>
+      <a href="/daftarmhsmbkm" class="list-group-item list-group-item-action my-2 py-2 ripple">
+        <i class="bi bi-table me-3"></i>
+        <span>Daftar Mahasiswa MBKM</span>
+      </a>
+      <a href="/mitra" class="list-group-item list-group-item-action my-2 py-2 ripple active">
+        <i class="bi bi-table me-3"></i>
+        <span>Daftar Mitra</span>
+      </a>
+      <a href="/daftarpengumuman" class="list-group-item list-group-item-action my-2 py-2 ripple">
+        <i class="bi bi-megaphone me-3"></i>
+        <span>Daftar Pengumuman</span>
+      </a>
+    </div>
+  </div>
+</nav>
+@endsection
+@section('navbar')
+<nav class="navbar navbar-light bg-light fixed-top navbar-expand-lg">
+  <div class="container-fluid">
+    <i id="navbtn" onclick="openNav()" class="openbtn bi bi-list"></i>
+    <a class="navbar-brand ms-3" href="#">
+      <img src="img/hehe.jfif" alt="" width="30" height="30">
+      <span><strong>Portal MBKM</strong></span>
+      <span><small>Dosen</small></span>
+    </a>
+    <a class="nav-link tombolnavbar" href="#">Tentang Aplikasi</a>
+    <a class="nav-link tombolnavbar" href="#"><i class="bi bi-megaphone-fill me-2"></i>Buat Pengumuman</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <ul class="navbar-nav navbar-nav-scroll ms-auto d-flex flex-row">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-bell"></i>
+            <span class="badge rounded-pill badge-notification bg-danger">1</span>
+          </a>
+          <ul class="dropdown-menu notifscroll  dropdown-menu-end" aria-labelledby="navbarSCrollingDropdown">
+            <li>
+              <a class="dropdown-item" href="#">
+                <div class="card small">
+                  <div class="card-body">
+                    <span class="tekskecil">INI NOTIFIKASI</span>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <div class="card small">
+                  <div class="card-body">
+                    <span class="tekskecil">INI NOTIFIKASI</span>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <div class="card small">
+                  <div class="card-body">
+                    <span class="tekskecil">INI NOTIFIKASI</span>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <div class="card small">
+                  <div class="card-body">
+                    <span class="tekskecil">INI NOTIFIKASI</span>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item text-primary" href="#">Tampilkan semua notifikasi</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown d-flex flex-row justify-content-end">
+          <a class="nav-link dropdown-toggle" href="#"id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <img class="rounded-circle" src="img/hehe.jfif" alt="" width="30" height="30">
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarSCrollingDropdown">
+            <li>
+              <a class="dropdown-item" href="#">
+                <i class="bi bi-person-badge me-2"></i>Profil
+              </a>
+            </li>
+            <li><hr class="dropdown-divider"></li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <i class="bi bi-box-arrow-left me-2"></i>Logout
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+@endsection
+@section('maincontent')
+<div class="container-fluid">
+  <div class="">
+    <span class="h1">Daftar Mitra MBKM</span>
+  </div>
+  <div class="container-fluid bg-info py-3 mt-3">
+    <div class="row mb-3">
+      <div class="col">
+        <div class="card">
+          <div class="card-header">
+            <p class="h3">Daftar Mitra</p>
+          </div>
+          <div class="card-body">
+            <form>
+              <div class="mb-3">
+                <div class="d-flex flex-row">
+                  <form action="{{ route('search') }}" method="GET">
+                  <input type="text" name="search" id="search" class="form-control me-2" placeholder="Search nama mitra..." value="{{ old('cari') }}">
+                  <button type="submit" class="btn btn-primary">
+                    <i class="bi bi-search"></i>
+                  </button>
+                  </form>
+                </div>
+              </div>
+            </form>
+            <button class="btn btn-primary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Tambah">
+                <i class="bi bi-plus-square"></i> <a href="{{ route('mitra.create') }}" style="color:white"> Tambah </a>
+              </button>
+            <table class="table table-bordered">
+              <tr>
+                <th width="20px">No.</th>
+                <th width="250px">KODE MITRA</th>
+                <th width="250px">NAMA MITRA</th>
+                <th width="100px">AKSI</th>
+              </tr>
+              <?php 
+              $mitras = DB::table('tbl_mitra')->get();
+              $no = 1;
+              ?>
+              @foreach($mitras as $mitra)
+                <tr class="rowclickable">
+                <td>{{$no++ }}</td> 
+                <td>{{$mitra->kode_mitra}}</td>
+                <td>{{$mitra->nama_mitra}}</td>
+                <td class="text-center">
+                    <button class="btn btn-warning" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Edit"><a href="/mitra/edit/{{ $mitra->kode_mitra }}" style="color:white"><i class="bi bi-pencil-square"></i></a>
+                      </button>
+                      <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('mitra.destroy', $mitra->kode_mitra) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Hapus"><a href="{{ route('mitra.destroy', $mitra->kode_mitra) }}" style="color:white;"><i class="bi bi-trash3"></i></a> </button>
+                    </form>
+                  </td>
+                </td>
+              </tr>
+                </td>
+              </tr>
+              @endforeach
+            </table>
+          </div>
+        </div>
+        <br/>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- MODAL HAPUS DAFTAR MITRA -->
+<div class="modal fade" id="modalhapus" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form>
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Hapus Mitra?</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <input class="inputid" type="text" id="inputid" value="">
+          <p id="namamhs" class=""></p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak</button>
+          <button type="submit" class="btn btn-danger">Hapus</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script>
+//message with toastr
+    @if(session()-> has('success'))
+        
+        toastr.success('{{ session('success') }}', 'BERHASIL!'); 
+
+    @elseif(session()-> has('error'))
+
+        toastr.error('{{ session('error') }}', 'GAGAL!'); 
+        
+    @endif
+</script>
+@endsection
---
+++ resources/views/mitra/tambahmitra.blade.php
@@ -0,0 +1,159 @@
+@extends('template.layouttempdsn')
+@section('navbar')
+<nav class="navbar navbar-light bg-light fixed-top navbar-expand-lg">
+  <div class="container-fluid">
+    <i id="navbtn" onclick="openNav()" class="openbtn bi bi-list"></i>
+    <a class="navbar-brand ms-3" href="#">
+      <img src="{{asset('/img/hehe.jfif')}}" alt="" width="30" height="30">
+      <span><strong>Portal MBKM</strong></span>
+      <span><small>Dosen</small></span>
+    </a>
+    <a class="nav-link tombolnavbar" href="#">Tentang Aplikasi</a>
+    <a class="nav-link tombolnavbar" href="#"><i class="bi bi-megaphone-fill me-2"></i>Buat Pengumuman</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <ul class="navbar-nav navbar-nav-scroll ms-auto d-flex flex-row">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-bell"></i>
+            <span class="badge rounded-pill badge-notification bg-danger">1</span>
+          </a>
+          <ul class="dropdown-menu notifscroll  dropdown-menu-end" aria-labelledby="navbarSCrollingDropdown">
+            <li>
+              <a class="dropdown-item" href="#">
+                <div class="card small">
+                  <div class="card-body">
+                    <span class="tekskecil">INI NOTIFIKASI</span>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <div class="card small">
+                  <div class="card-body">
+                    <span class="tekskecil">INI NOTIFIKASI</span>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <div class="card small">
+                  <div class="card-body">
+                    <span class="tekskecil">INI NOTIFIKASI</span>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <div class="card small">
+                  <div class="card-body">
+                    <span class="tekskecil">INI NOTIFIKASI</span>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item text-primary" href="#">Tampilkan semua notifikasi</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown d-flex flex-row justify-content-end">
+          <a class="nav-link dropdown-toggle" href="#"id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <img class="rounded-circle" src="{{asset('/img/hehe.jfif')}}" alt="" width="30" height="30">
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarSCrollingDropdown">
+            <li>
+              <a class="dropdown-item" href="#">
+                <i class="bi bi-person-badge me-2"></i>Profil
+              </a>
+            </li>
+            <li><hr class="dropdown-divider"></li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <i class="bi bi-box-arrow-left me-2"></i>Logout
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+@endsection
+@section('maincontent')
+<div class="container-fluid">
+  <div class="">
+    <span class="h1">Daftar Mitra</span>
+  </div>
+  <div class="container-fluid bg-info py-5 mt-5">
+    <div class="row justify-content-center">
+    <div class="row mb-6">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            @if(session('alert'))
+            <p class="text-danger">
+              {{session('alert')}}
+            </p>
+            @endif
+            <form method="post" action="{{ route('mitra.store') }}" class="needs-validation" novalidate>
+                @csrf
+                <div class="form-group">
+                  <label for="kode_mitra" class="form-label">Kode Mitra</label>
+                  <input name="kode_mitra" type="text" id="kode_mitra" class="form-control" placeholder="Masukkan Kode Mitra" required>
+                  <div class="invalid-feedback">Harus masukkan Kode Mitra</div>
+                </div>
+                <div class="form-group">
+                  <label for="nama_mitra" class="form-label">Nama Mitra</label>
+                  <input name="nama_mitra" type="text" id="nama_mitra" class="form-control" placeholder="Masukkan Nama Mitra" required>
+                  <div class="invalid-feedback">Harus masukkan Nama Mitra</div>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                    <button class="btn btn-warning" value="Go Back"><a href="/mitra" style="color:white"> Kembali </a></button>
+                </div>
+            </form>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@if ($message = Session::get('success'))
+<div class="alert alert-success">
+    <p>{{ $message }}</p>
+</div>
+@endif
+
+
+<script>
+  (function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+</script>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
